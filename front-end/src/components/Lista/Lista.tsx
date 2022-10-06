887191fa0eb24030d384b766e45c037052cae157
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2022-10-06 17:11:07 -0300

    Iniciando lista de conteudo

---
+++ front-end/src/components/Lista/Lista.tsx
@@ -0,0 +1,16 @@
+import { Foto, ItemLista, ListaStyled } from "./Lista.style";
+
+const Lista = () =>{
+    return(
+        <ListaStyled>
+            <ItemLista>
+                <Foto src="#">
+
+                </Foto>
+            </ItemLista>
+
+        </ListaStyled>
+    )
+}
+
+export default Lista;
