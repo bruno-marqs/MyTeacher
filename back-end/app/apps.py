9f01499628caed8852f9fe65e0574b8490163ea6
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2022-10-09 12:12:49 -0300

    modificando app

--- back-end/app/apps.py
+++ back-end/app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TeacherConfig(AppConfig):
+class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'teacher'
+    name = 'app'
