74e322a6467e9416948668a4a2701752be24f8e0
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2022-10-09 16:26:49 -0300

    serializer/model/view/ class Professor

--- back-end/myteacher/urls.py
+++ back-end/myteacher/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from app.views import HomeApiView
+# from app.views import HomeApiView
+from teacher.views import ProfessorAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomeApiView.as_view())
+    # path('', HomeApiView.as_view()),
+    path('professores/', ProfessorAPIView.as_view())
 ]
