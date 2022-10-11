15d83c19f7adb3102679cd460ceeacfa06fd46fa
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2022-10-11 11:55:55 -0300

    criando hooks index

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -2,57 +2,14 @@ import { Box } from '@mui/material'
 import type { NextPage } from 'next'
 import { Professor } from '../src/@types/professor'
 import Lista from '../src/components/Lista/Lista'
+import { useIndex } from '../src/hooks/pages/useIndex'
 
 const Home: NextPage = () => {
-  const professores: Professor[] = [
-    {
-      id: 1,
-      nome: 'Bruno Marques',
-      foto: "https://github.com/o-Drive.png",
-      descricao: "Descrição do professor 1",
-      valor_hora: 100
-    },
-    {
-      id: 2,
-      nome: 'Kelvin Argolo',
-      foto: "https://github.com/Kelvin4rgolo.png",
-      descricao: "Descrição do professor 2",
-      valor_hora: 200
-    },
-    {
-      id: 3,
-      nome: 'Lucas Mendes',
-      foto: "https://github.com/Luckaszfsa.png",
-      descricao: "Descrição do professor 3",
-      valor_hora: 300
-    },
-    {
-      id: 4,
-      nome: 'Vinicius de Assis',
-      foto: "https://github.com/ViniciusDevAssis.png",
-      descricao: "Descrição do professor 4",
-      valor_hora: 400
-    },
-    {
-      id: 5,
-      nome: 'José Marcos',
-      foto: "https://github.com/socramcz.png",
-      descricao: "Descrição do professor 5",
-      valor_hora: 500
-    },
-    {
-      id: 6,
-      nome: 'Izabela Pereira',
-      foto: "https://github.com/izabela-pereira.png",
-      descricao: "Descrição do professor 6",
-      valor_hora: 600
-    },
-
-  ]
+  const { listaProfessores } = useIndex(); 
 
   return (
     <Box sx={{ backgroundColor: 'secondary.main' }}>
-      <Lista professores={professores}></Lista>
+      <Lista professores={listaProfessores}></Lista>
     </Box>
   )
 }
---
+++ front-end/src/hooks/pages/useIndex.ts
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { Professor } from "../../@types/professor";
+
+export function useIndex() {
+    const [listaProfessores, setListaProfessores] = useState<Professor[]>([
+        {
+            id: 1,
+            nome: 'Bruno Marques',
+            foto: "https://github.com/o-Drive.png",
+            descricao: "Descrição do professor 1",
+            valor_hora: 100
+          },
+          {
+            id: 2,
+            nome: 'Kelvin Argolo',
+            foto: "https://github.com/Kelvin4rgolo.png",
+            descricao: "Descrição do professor 2",
+            valor_hora: 200
+          },
+          {
+            id: 3,
+            nome: 'Lucas Mendes',
+            foto: "https://github.com/Luckaszfsa.png",
+            descricao: "Descrição do professor 3",
+            valor_hora: 300
+          },
+          {
+            id: 4,
+            nome: 'Vinicius de Assis',
+            foto: "https://github.com/ViniciusDevAssis.png",
+            descricao: "Descrição do professor 4",
+            valor_hora: 400
+          },
+          {
+            id: 5,
+            nome: 'José Marcos',
+            foto: "https://github.com/socramcz.png",
+            descricao: "Descrição do professor 5",
+            valor_hora: 500
+          },
+          {
+            id: 6,
+            nome: 'Izabela Pereira',
+            foto: "https://github.com/izabela-pereira.png",
+            descricao: "Descrição do professor 6",
+            valor_hora: 600
+          }
+    ]);
+
+    return {
+        listaProfessores
+    }
+}
