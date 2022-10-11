5d787c4beadeb4efbea35346809e3aa37457abd0
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2022-10-11 14:10:50 -0300

    limpando form

--- front-end/src/hooks/pages/useIndex.ts
+++ front-end/src/hooks/pages/useIndex.ts
@@ -15,6 +15,10 @@ export function useIndex() {
         })
     }, []);
 
+    useEffect(() => {
+        limparFormulario();
+    }, [professorSelecionado])
+
     function marcarAula(){
         if (professorSelecionado !== null){
             if (validarDadosAula()){
@@ -38,6 +42,11 @@ export function useIndex() {
         return nome.length > 0 && email.length > 0;
     }
 
+    function limparFormulario(){
+        setNome('');
+        setEmail('');
+    }
+
     return {
         listaProfessores,
         nome,
@@ -48,6 +57,7 @@ export function useIndex() {
         setProfessorSelecionado,
         marcarAula,
         mensagem,
-        setMensagem
+        setMensagem, 
+        limparFormulario
     }
 }
